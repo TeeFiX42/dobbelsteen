0c1b86488e35e892dfdf8ffcd790af52a87e1bb1
TeeFiX42 <47397709+TeeFiX42@users.noreply.github.com>
2021-01-16 13:05:31 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,61 @@
+let dobbelsteengetal = 0
+input.onGesture(Gesture.Shake, function () {
+    dobbelsteengetal = randint(1, 6)
+    if (dobbelsteengetal == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (dobbelsteengetal == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (dobbelsteengetal == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (dobbelsteengetal == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (dobbelsteengetal == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (dobbelsteengetal == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
